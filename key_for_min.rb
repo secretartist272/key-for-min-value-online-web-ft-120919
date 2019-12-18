7cacf2ef353f5b8e20be8aa7924237fbd10c303f
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 04:27:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,29 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  current_minimum_val = nil 
+  current_key_assoc_w_min_val = nil 
+  #the starting value and key assignment is nil
+  #because we haven't gone through the hash yet
+  name_hash.each do |key, value|
+  #now we're starting to iterate through the hash
+  #and if the current_min_val is nil, we've just
+  #started iterating. So then we make the 
+  #current_min_val equal to value (the val itera-
+  #tion we're on, and take the assoc'd key)
+    if current_minimum_val == nil 
+      current_minimum_val = value 
+      current_key_assoc_w_min_val = key 
+    else 
+    #we keep iterating and if the current min val
+    #i greater than our old value we replace it
+    #with the new lower value and return the assoc'd
+    #key. 
+      if current_minimum_val > value 
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key 
+      end
+    end
+  end 
+  current_key_assoc_w_min_val 
 end
