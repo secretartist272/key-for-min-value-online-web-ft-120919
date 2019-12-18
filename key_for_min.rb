5d46ec261a8a34428e6feba196e7811030dbfb37
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 04:28:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,19 +4,11 @@
 def key_for_min_value(name_hash)
   current_minimum_val = nil 
   current_key_assoc_w_min_val = nil 
-  #the starting value and key assignment is nil
-  #because we haven't gone through the hash yet
   name_hash.each do |key, value|
-  #now we're starting to iterate through the hash
-  #and if the current_min_val is nil, we've just
-  #started iterating. So then we make the 
-  #current_min_val equal to value (the val itera-
-  #tion we're on, and take the assoc'd key)
     if current_minimum_val == nil 
       current_minimum_val = value 
       current_key_assoc_w_min_val = key 
     else 
-     
       if current_minimum_val > value 
         current_minimum_val = value
         current_key_assoc_w_min_val = key 
